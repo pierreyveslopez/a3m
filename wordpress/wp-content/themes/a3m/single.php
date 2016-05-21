1816a782872f26b6007412f4cad594eb4a556911
Pierre-Yves LOPEZ <pierreyves.lopez@gmail.com>
2016-05-21 17:53:06 +0200

    [#A3M006]
    Dynamisation du bloc « articles relatifs » dans la rubrique enjeux +
    template single

---
+++ wordpress/wp-content/themes/a3m/single.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  Template Name: Pages enjeux
+ */
+ 
+    get_header();?>
+    <div id="mainContent">
+    <?php custom_breadcrumbs($post); ?>
+    <?php get_sidebar(); ?>
+      <div class="pageContent">
+        <div class="pure-u-1 pure-u-md-1-1 l-box hp-actu">
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <h2 class="titlePAge"><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+          <?php endwhile; endif; wp_reset_query(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php get_footer();?>
--- wordpress/wp-content/themes/a3m/template-enjeux.php
+++ wordpress/wp-content/themes/a3m/template-enjeux.php
@@ -10,6 +10,7 @@
       <div class="pageContent">
         <div class="pure-u-1 pure-u-md-1-1 l-box hp-actu">
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <?php $postName = $post->post_title;?>
           <?php $subtitle = get_field('sous_titre');
             if($subtitle != ''){
           ?>
@@ -23,23 +24,35 @@
               <h2 class="titlePAge"><?php the_title(); ?></h2>
             <?php } ?>
             <?php the_content(); ?>
-          <?php endwhile; endif; ?>
+          <?php endwhile; endif; wp_reset_query(); ?>
+          <?php
+            query_posts(
+              array(
+                'post_type' => 'post',
+                'showposts' => 20,
+                'tax_query' => array(
+                  array(
+                    'taxonomy' => 'post_tag',
+                    'terms' => $postName,
+                    'field' => 'name'
+                  )
+                ),
+                
+                'orderby' => 'date',
+                'order' => 'DSC'
+              )
+            );
+            if ( have_posts() ) : 
+          ?>
           <div class="articlesRelatifs">
             <h3>Liste des articles relatifs :</h3>
               <ul>
-                <li><span class="date">26/02/2014 :</span> <span class="title"><a href="#" title="">La certification des installations de recyclage progresse</a></span></li>
-                <li><span class="date">13/01/2014 :</span> <span class="title"><a href="#" title="">ADEME - premières conclusions de l’étude sur la compétitivité des filières de recyclage</a></span></li>
-                <li><span class="date">22/11/2013 :</span> <span class="title"><a href="#" title="">Les acteurs de la filières du recyclage s’engagent</a></span></li>
-                <li><span class="date">30/09/2013 :</span> <span class="title"><a href="#" title="">Feuille de route de la conférence environnementale 2013</a></span></li>
-                <li><span class="date">17/07/2013 :</span> <span class="title"><a href="#" title="">Réunion d’information FEDEM - Recyclage - 4 juillet 2013</a></span></li>
-                <li><span class="date">27/02/2013 :</span> <span class="title"><a href="#" title="">ADEME - Lancement de l’étude compétitivité et filières de recyclage</a></span></li>
-                <li><span class="date">25/10/2012 :</span> <span class="title"><a href="#" title="">Colloque ADEME Filières et Recyclage : L’économie circulaire porteuse d’avenir</a></span></li>
-                <li><span class="date">10/07/2012 :</span> <span class="title"><a href="#" title="">Réunion d’échanges et de travail sur le recyclage et les conditions d’une meilleure compétitivité pour l’industrie des métaux non ferreux - 9 juillet 2012</a></span></li>
-                <li><span class="date">20/04/2012 :</span> <span class="title"><a href="#" title="">Observatoire du recyclage</a></span></li>
-                <li><span class="date">25/01/2012 :</span> <span class="title"><a href="#" title="">Indicateurs de recyclage : Les propositions d’Eurometaux</a></span></li>
-                <li><span class="date">15/12/2011 :</span> <span class="title"><a href="#" title="">L’Union des industries du recyclage : Les déchets, matières premières de nos industries</a></span></li>
+                <?php while ( have_posts() ) : the_post(); ?>
+                <li><span class="date"><?php echo get_the_date('d/m/Y'); ?>&nbsp;: </span> <span class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title();?> - A3M"><?php the_title();?></a></span></li>
+                <?php endwhile; wp_reset_query();?>
               </ul>            
           </div>
+          <?php endif; ?>
         </div>
       </div>
     </div>
