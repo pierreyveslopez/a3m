256f6fa43be155311dbfbdf47a61ef515db71159
Mona <mona@red-cranberry.com>
2016-05-29 19:13:18 +0200

    Ajout du plan du site

--- wordpress/wp-content/themes/a3m/footer.php
+++ wordpress/wp-content/themes/a3m/footer.php
@@ -2,7 +2,7 @@
       <div class="pure-g">
         <nav id="footerNavigation" class="pure-u-1">
           <ul>
-            <li><a href="#">Plan du site</a></li>
+            <li><a href="<?php bloginfo(template_url); ?>/plan-du-site">Plan du site</a></li>
             <li><a href="#">Contact</a></li>
             <li><a href="#">Mentions légales</a></li>
           </ul>
