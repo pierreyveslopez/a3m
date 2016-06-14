131ba23c5bbe9172d90d344d9efad68a606b2d82
Pierre-Yves LOPEZ <pierreyves.lopez@gmail.com>
2016-06-14 11:45:50 +0200

    Ajout page adherent + Fix menu

--- wordpress/wp-content/themes/a3m/template-espace-adherent.php
+++ wordpress/wp-content/themes/a3m/template-espace-adherent.php
@@ -54,34 +54,63 @@
         <div class="col3--espaceAdherents">
           <div class="col">
             <div class="blc">
-              <img src="<?php bloginfo(template_url); ?>/img/content/img-espace-adherent.jpg" alt="" />
-              <h2 class="angle lightblue">A la une</h2>
-              <h3>Les métaux recyclables au coeur de l'économie</h3>
-              <p><strong>Dans le cadre des actions de promotion de la profession, A3M publie ce mois-ci une plaquette dédiée à l'économie circulaire</strong><br/>
-              Cette plaquette s'inscrit dans la dynamique actuelle autour de cette thématique et vise à valoriser à la fois l'engagement de notre profession, sa place dans le cycle de vie des matériaux ainsi que la performance des produits métalliques en faveur de la transition écologique</p>
-              <p><a class="btn" href="#" title="">Voir plus -></a></p>
+              <?php 
+              $the_query = new WP_Query( array( 'page_id' => 161 )  );
+              while ( $the_query->have_posts() ) : $the_query->the_post();
+              ?>
+                <?php echo get_the_post_thumbnail( $page->ID, 'medium' ); ?>
+                <h2 class="angle lightblue"><?php the_title(); ?></h2>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>" class="btn">Voir plus  --></a>
+
+              <?php
+                endwhile; wp_reset_query();
+              ?>
+            
             </div>
             <div class="blc">
-              <img src="<?php bloginfo(template_url); ?>/img/content/img-espace-adherent.jpg" alt="" />
-              <h2 class="angle yellow">Parole aux adhérents</h2>
-              <h3>Emeris graphite & carbone double</h3>
-              <p>Anne Fauconnier, Directrice de la Communication Corporate & Interne d’Emeris, souligne par ailleurs la réussite de la finalisation du chantier en temps et en heure et surtout sans accident: « la construction de la ligne de production a duré environ un an, il n’y a eu aucun dépassement de délai, aucun dépassement budgétaire, et surtout, aucun accident, ce dont on se félicite. »</p>
-              <p><a class="btn" href="#" title="">Voir plus -></a></p>
+              <?php 
+              $the_query = new WP_Query( array( 'page_id' => 163 )  );
+              while ( $the_query->have_posts() ) : $the_query->the_post();
+              ?>
+                <?php echo get_the_post_thumbnail( $page->ID, 'medium' ); ?>
+                <h2 class="angle yellow"><?php the_title(); ?></h2>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>" class="btn">Voir plus  --></a>
+
+              <?php
+                endwhile; wp_reset_query();
+              ?>
             </div>
           </div>
           <div class="col">
             <div class="blc">
-              <img src="<?php bloginfo(template_url); ?>/img/content/img-espace-adherent.jpg" alt="" />
-              <h2 class="angle lightgreen">Environnement</h2>
-              <h3>Réunion d'information et d'échanges d'expériences A3M</h3>
-              <p><strong>Le 17 mars prochain, A3M organise une réunion d'information et d'échanges d'expériences sur le thème "L'écologie industrielle au coeur des projets industriels de territoire.</strong><br/>
-              Afin de réduire leurs coûts de production, les industriels du secteur de la métallurgie sont en recherche perpétuelle de solutions qui leur permettent de valoriser au mieux leurs déchets ainsi que l’énergie fatale et les gaz issus de leurs procédés.</p>
-              <p><a class="btn" href="#" title="">Voir plus -></a></p>
+               <?php 
+              $the_query = new WP_Query( array( 'page_id' => 169 )  );
+              while ( $the_query->have_posts() ) : $the_query->the_post();
+              ?>
+                <?php echo get_the_post_thumbnail( $page->ID, 'medium' ); ?>
+                <h2 class="angle lightgreen"><?php the_title(); ?></h2>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>" class="btn">Voir plus  --></a>
+
+              <?php
+                endwhile; wp_reset_query();
+              ?>         
             </div>
             <div class="blc">
-              <h2 class="angle">Note de lecture</h2>
-              <p>Suite au Conseil européen des 23 et 24 octobre qui a validé le principe « d’un instrument visant à stabiliser le marché » européen du carbone, les commissions environnement et industrie du Parlement européen ont commencé l’examen de la proposition de texte de la Commission européenne.</p>
-              <p><a class="btn" href="#" title="">Voir plus -></a></p>
+              <?php 
+              $the_query = new WP_Query( array( 'page_id' => 173 )  );
+              while ( $the_query->have_posts() ) : $the_query->the_post();
+              ?>
+                <?php echo get_the_post_thumbnail( $page->ID, 'medium' ); ?>
+                <h2 class="angle"><?php the_title(); ?></h2>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>" class="btn">Voir plus  --></a>
+
+              <?php
+                endwhile; wp_reset_query();
+              ?>         
             </div>
           </div>
           <div class="col">
